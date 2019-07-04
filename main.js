2223a413082853a00ac235b93831116f1047cb95
Melinda McRae <Melinda.J.McRae@gmail.com>
2019-07-04 15:03:14 -0600

    add submit guess-in progress

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//changes the min/max input to reflect in HTML
+
 var minInput = document.querySelector("#min-input");
 var maxInput = document.querySelector("#max-input");
 var updateButton = document.querySelector("#update-button");
@@ -12,6 +14,36 @@ function changeNumbers(e) {
 	maxNumber.innerText = maxInput.value;
 }
 
+// random value generated
 function getRandom(minInput, maxInput) {
   return Math.ceil(Math.random() * (maxInput - minInput) + minInput)
 }
+
+// var getRandom = math.ceil(Math.random() * (maxInput - minInput) + minInput)
+
+var submitButton = document.querySelector("#submit-button");
+
+var guessInput1 = document.getElementById("guess-input-1").value;
+var guessInput2 = document.getElementById("guess-input-2").value;
+
+submitButton.addEventListener("click", displayGuessResults);
+
+function displayGuessResults() {
+	if (guessInput1 == getRandom) 
+	{
+		alert("BOOM");
+	}
+
+	else if (guessInput1 > getRandom)
+	{
+		alert("that's too high");
+	}
+
+	else (guessInput1 < getRandom)
+	{
+		alert("that's too low");
+	}
+}
+
+
+ 
